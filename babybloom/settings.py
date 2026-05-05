16c2639663cafe9a7de0d18d80171e7fae303b96
Thomas Antony <nivint6@gmail.com>
2026-05-05 12:05:23 +0530

    feat: initialize project settings and add required dependencies for Django, DRF, JWT, and CORS support

--- babybloom/settings.py
+++ babybloom/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import dj_database_url
+
 import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,12 +90,11 @@ WSGI_APPLICATION = 'babybloom.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'default':dj_database_url.config(default='sqlite:///db.sqlite3')
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
+
 
 
 # Password validation
